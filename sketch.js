54e51cb75e63a4861d347f9bc8684a55a7a0486c
op-alash-yt <76893066+op-alash-yt@users.noreply.github.com>
2021-01-09 09:59:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var fr , mr;
+
+function setup() {
+  createCanvas(800,400);
+fr = createSprite(400, 200, 50, 50);
+mr = createSprite(200,200,50,50);
+fr.shapeColor = "green";
+mr.shapeColor = "green";
+}
+
+function draw() {
+  background(255,255,255); 
+  mr.x = mouseX;
+  mr.y = mouseY;
+
+if (mr.x - fr.x < fr.width/2 + mr.width/2 && fr.x - mr.x < fr.width/2 + mr.width/2 && mr.y - fr.y < fr.height/2 + mr.width/2 && fr.y - mr.y < fr.height/2 + mr.height/2){
+  fr.shapeColor = "red";
+  mr.shapeColor = "red";
+}
+  
+else {
+fr.shapeColor = "green";
+mr.shapeColor = "green";
+}
+
+
+  drawSprites();
+}
